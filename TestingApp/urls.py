065e257887e4f06f2c3cb0e16725b7308edb6803
Ambar Cano <ambarcano@gmail.com>
2022-06-18 15:39:59 -0400

    added model part

--- TestingApp/urls.py
+++ TestingApp/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from api.router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.authtoken')),
+    # path('api/', include(router.urls)),
 ]
--- api/models.py
+++ api/models.py
@@ -8,3 +8,29 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Test(models.Model):
+    title = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='chef', max_length = 255)
+#temporarily set to charfield in order to research how to sequential number entries
+    version_number = models.CharField(max_length=3)
+    ingredients = models.TextField(max_length=500)
+    recipe = models.TextField(max_length=800)  
+    image = models.ImageField()
+    outside_notes = models.CharField(max_length=400)
+    final_notes = models.CharField(max_length=400)
+    adjustments = models.CharField(max_length=455)
+    feedback_link = models.URLField(max_length=255)
+#temporarily set to CharField, researching how to connect with tags component from react FE repo
+    tags = models.CharField(max_length=255)
+    chef = models.ForeignKey('User', on_delete=models.CASCADE, related_name='chef', max_length = 255)
+    variation_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    successful_variation = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} by {self.chef}"
+
+
+class Recipe(models.Model):
+    pass
